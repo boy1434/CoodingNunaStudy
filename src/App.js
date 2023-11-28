14537dd972bfa3885a3f4bd21c8b07e322631ab0
boy1434@naver.com <boy1434@naver.com>
2023-11-28 22:29:12 +0900

    useEffect 두번째 인자 사용

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-    console.log("run all the time");
-    const RunOnce = () => {
-      console.log("run once");
-    };
-    useEffect(RunOnce, []);
+  const onChange = (event) => setKeyword(event.target.value);
+    useEffect(() => {
+      console.log("i run only once");
+    }, []);
+    useEffect(() =>{
+      console.log("i run when 'keyword' changes", keyword);
+    }, [keyword]);
+    useEffect(() =>{
+      console.log("i run when 'counter' changes", keyword);
+    }, [counter]);
   return (
     <div>
+      <input 
+        value={keyword} 
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here"
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click</button>
     </div>
