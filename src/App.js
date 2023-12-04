3ac7e172473cef15815ea63010fe9fc4d017ef50
boy1434@naver.com <boy1434@naver.com>
2023-12-04 22:19:15 +0900

    gh-page(deploy)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
+import Detail from "./routes/Detail";
 import Home from "./routes/Home"
 function App() {
   return <Router>
     <Switch>
+      <Route path="/movie/:id">
+        <Detail />
+      </Route>
       <Route path="/">
         <Home />
       </Route>
